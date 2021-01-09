36e56ab4e605b647bea88f6a2d5b927d4f47ae6a
fadyehabamer <fadyamer45@gmail.com>
2021-01-09 03:19:41 +0200

    Add files via upload

---
+++ public/js/app.js
@@ -0,0 +1,9 @@
+function imgSelected(img){
+  document.querySelector('.starbuks').src = img;
+}
+
+
+function channgCircleColor(color){
+  let circle = document.querySelector('.circle');
+  circle.style.background = color;
+}
